98ffde73bb24b3a0510b270119678a15b01f34d1
Simon Symeonidis <lethaljellybean@gmail.com>
2020-09-01 00:23:54 -0400

    Add consent segment typing in header file

--- include/consent_string.hrl
+++ include/consent_string.hrl
@@ -22,10 +22,37 @@
 
     max_vendor_id             :: non_neg_integer(),
     encoding_type             :: 0..1,
-    vendors                   :: vendor_bit_field() | vendor_range(),
+    vendors                   :: vendor_bit_field() | vendor_range(), %% TODO: refactor this into range_or_bitfield
 
     vendor_legitimate_interests :: vendor_legitimate_interests(),
-    publisher_restrictions      :: publisher_restrictions()
+    publisher_restrictions      :: publisher_restrictions(),
+
+    segments :: list(consent_segment())
+}).
+
+-record(consent_segment_entry_disclosed_vendors, {
+    max_vendor_id :: pos_integer(),
+    entries       :: range_or_bitfield()
+}).
+
+-record(consent_segment_entry_allowed_vendors, {
+}).
+
+-record(consent_segment_entry_publisher_purposes, {
+}).
+
+-record(consent_segment, {
+    type  :: consent_segment_type(),
+    entry :: consent_segment_entry()
+}).
+
+-record(entry_range, {
+    num_entries :: pos_integer(),
+    entries     :: list()
+}).
+
+-record(entry_bitfield, {
+    field :: binary()
 }).
 
 -record(vendor_bit_field, {
@@ -55,6 +82,7 @@
 
 -record(publisher_restrictions_entry, {
     restriction_type :: restriction_type(),
+    purpose_id       :: pos_integer(),
     num_entries      :: pos_integer(),
     entries          :: list()
 }).
@@ -66,8 +94,28 @@
 
 -type consent() :: #consent {}.
 
+-type consent_segment() :: #consent_segment {}.
+
+-type consent_segment_default() :: 0.
+-type consent_segment_disclosed_vendors() :: 1.
+-type consent_segment_allowed_vendors() :: 2.
+-type consent_segment_publisher_tc() :: 3.
+
+-type consent_segment_type() :: consent_segment_default() |
+                                consent_segment_disclosed_vendors() |
+                                consent_segment_allowed_vendors() |
+                                consent_segment_publisher_tc().
+
+-type consent_segment_entry() :: consent_segment_entry_disclosed_vendors() |
+                                 consent_segment_entry_allowed_vendors() |
+                                 consent_segment_entry_publisher_purposes().
+
 -type publisher_restrictions()       :: #publisher_restrictions {}.
 -type publisher_restrictions_entry() :: #publisher_restrictions_entry {}.
+
+-type entry_range() :: #entry_range {}.
+-type entry_bitfield() :: #entry_bitfield {}.
+-type range_or_bitfield() :: entry_range() | entry_bitfield().
 
 -type restriction_not_allowed_by_publisher() :: 0.
 -type restriction_require_consent() :: 1.
@@ -78,6 +126,10 @@
                             restriction_require_consent() |
                             restriction_require_legitimate_interest() |
                             restriction_unknown().
+
+-type consent_segment_entry_disclosed_vendors()  :: #consent_segment_entry_disclosed_vendors {}.
+-type consent_segment_entry_allowed_vendors()    :: #consent_segment_entry_allowed_vendors {}.
+-type consent_segment_entry_publisher_purposes() :: #consent_segment_entry_publisher_purposes {}.
 
 -type vendor_bit_field() :: #vendor_bit_field {}.
 -type vendor_range() :: #vendor_range {}.
